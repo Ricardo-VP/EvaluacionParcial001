3ff9c09d5a63ba44be0cad33263c25966f014e97
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-12-03 12:00:44 -0500

    Agregar archivos de proyecto.

---
+++ EvaluacionParcial001/clases/Ej1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionParcial001.clases
+{
+    class Ej1
+        {
+        static double formula()
+        {
+            double resultado = 0;
+
+        }
+
+        static int factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n * factorial(n - 1);
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+
+    
+
+}
